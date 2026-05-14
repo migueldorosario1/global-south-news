9ba9d0d31bdeaae5ae349cdd7abcbd08ab488d0c
Miguel do Rosario <migueldorosario@gmail.com>
2026-05-14 07:59:50 -0300

    Fix: atualizar URL do site no astro config para corrigir botões de compartilhamento

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://www.riocarta.com',
     integrations: [mdx(), sitemap(), react()],
     fonts: [
         {
