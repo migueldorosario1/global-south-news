3fbb4bd7ad405e3cda79d1d10abdf383289ff965
Miguel do Rosario <migueldorosario@gmail.com>
2026-05-16 15:29:15 -0300

    Initial GSN commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Rio Carta';
-export const SITE_DESCRIPTION = 'Portal de Notícias do Rio de Janeiro, por Miguel do Rosário';
+export const SITE_TITLE = 'Global South News';
+export const SITE_DESCRIPTION = 'Notícias de Geopolítica e Política do Sul Global, por Miguel do Rosário';
